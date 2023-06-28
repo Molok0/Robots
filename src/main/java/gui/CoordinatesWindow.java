f3be9194e3bde0bbcd6414658ff423677c3ad518
DmitryProshin <do.proshin@yandex.ru>
2023-06-28 15:33:36 +0500

    completed localization

--- src/main/java/gui/CoordinatesWindow.java
+++ src/main/java/gui/CoordinatesWindow.java
@@ -1,22 +1,32 @@
 package gui;
 
+import localization.LanguageAdapter;
+import localization.LanguageModel;
+import localization.LanguageUpdate;
+import localization.Localization;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
-public class CoordinatesWindow extends JInternalFrame implements Observer {
+public class CoordinatesWindow extends JInternalFrame implements Observer, LanguageUpdate {
     private DataModel m_model;
     private JTextArea coordinate;
 
+    private LanguageModel languageModel = LanguageModel.getInstance();
+
     public CoordinatesWindow(DataModel model) {
-        super("Координаты робота", true, true, true, true);
+        super(Localization.get("window_coordinates"), true, true, true, true);
         m_model = model;
         m_model.addObserver(this);
 
         JPanel panel = new JPanel(new BorderLayout());
         coordinate = new JTextArea();
         coordinate.setPreferredSize(new Dimension(250, 80));
+
+        addPropertyChangeListener(new LanguageAdapter(this, languageModel));
+
         panel.add(coordinate, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
@@ -35,4 +45,8 @@ public class CoordinatesWindow extends JInternalFrame implements Observer {
         coordinate.setText("X: " + String.valueOf(m_model.getRobotPositionX()) + "\n" + "Y: " + String.valueOf(m_model.getRobotPositionY()));
     }
 
+    @Override
+    public void changeLanguage() {
+        setTitle(Localization.get("window_coordinates"));
+    }
 }
--- src/main/java/gui/GameWindow.java
+++ src/main/java/gui/GameWindow.java
@@ -1,20 +1,32 @@
 package gui;
 
+import localization.LanguageAdapter;
+import localization.LanguageModel;
+import localization.LanguageUpdate;
+import localization.Localization;
+
 import java.awt.BorderLayout;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 
-public class GameWindow extends JInternalFrame {
+public class GameWindow extends JInternalFrame implements LanguageUpdate {
     private final GameVisualizer m_visualizer;
     private DataModel m_model;
+    private LanguageModel languageModel = LanguageModel.getInstance();
 
     public GameWindow(DataModel model) {
-        super("Игровое поле", true, true, true, true);
+        super(Localization.get("window_game"), true, true, true, true);
         m_visualizer = new GameVisualizer(model);
         JPanel panel = new JPanel(new BorderLayout());
+        addPropertyChangeListener(new LanguageAdapter(this, languageModel));
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+
+    @Override
+    public void changeLanguage() {
+        setTitle(Localization.get("window_game"));
     }
 }
